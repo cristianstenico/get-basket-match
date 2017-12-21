42d3d21330b11b7e00844328992db0a56ae2cd08
Cristian Stenico <cristian.stenico@praim.com>
2017-12-21 15:21:36 +0100

    fip cambia sito a metà stagione, fuck

--- Game.py
+++ Game.py
@@ -6,20 +6,19 @@ romeTimeZone = timezone('Europe/Rome')
 
 
 class Game:
-    def __init__(self, league, gameData, place):
+    def __init__(self, league, teamA, teamB, result, place, dateData):
         self.league = [league]
-        self.teamA = gameData.xpath('.//div/a')[0].text.strip()
-        self.teamB = gameData.xpath('.//div/a')[1].text.strip()
-        self.place = place
+        self.teamA = teamA
+        self.teamB = teamB
         self.isGardolo = self.teamA == 'BC GARDOLO' or self.teamB == 'BC GARDOLO' or self.teamA == 'MB GARDOLO' or self.teamB == 'MB GARDOLO'
         self.isUnder20 = self.teamA == 'BC GARDOLO U20' or self.teamB == 'BC GARDOLO U20'
         if self.isGardolo or self.isUnder20:
-            dateData = gameData.xpath('.//td[@class="risTr1P"]/font')
             # Se è presente la data, la partita non è ancora stata giocata,
             # posso quindi salvarla su calendar
-            if len(dateData) > 0:
+            if result == '-':
                 self.futureGame = True
-                data = re.fullmatch('(?P<day>\d\d)/(?P<month>\d\d)/(?P<year>\d\d\d\d) - (?P<time>\d\d:\d\d)', dateData[0].text.strip())
+                self.place = place
+                data = re.fullmatch('(?P<day>\d\d)/(?P<month>\d\d)/(?P<year>\d\d\d\d) - (?P<time>\d\d:\d\d)', dateData)
                 self.gameday = date(int(data.group('year')), int(data.group('month')), int(data.group('day')))
                 self.time = time(*map(int, data.group('time').split(':')))
                 if self.isUnder20:
@@ -29,4 +28,4 @@ class Game:
                         self.league = ['Under 20']
             else:
                 self.futureGame = False
-                self.result = gameData.xpath('.//td[@class="risTr1P"]')[0].text.strip()
+                self.result = result
--- get-basket-matches.py
+++ get-basket-matches.py
@@ -28,46 +28,69 @@ localEvents = []
 service = Service()
 
 # Scarico la homepage del sito Fip Trentino
-home = requests.get('http://fip.it/risultati.asp?IDRegione=TN&com=RTN&IDProvincia=TN')
+home = requests.post('http://fip.it/FipWeb/ajaxRisultatiGetMenuCampionati.aspx', data={'ar':'1', 'com':'RTN', 'IDProvincia':'TN', 'IDRegione':'TN','turno':'1'})
 
 # Cerco i vari link delle categorie
-for m in re.finditer('getCampionato\(\'RTN\', \'(?P<campionato>.+)\', \'(?P<fase>.+)\', \'(?P<codice>.+)\', (?P<andata>\d), (?P<turno>\d)\)', home.text):
+for m in re.finditer('getRisultatiPartite\(\'RTN\', \'(?P<sesso>[MF])\', \'(?P<campionato>[^\']+)\', \'(?P<fase>[^\']+)\', \'(?P<codice>[^\']+)\', \'(?P<andata>\d)\', \'(?P<turno>\d)\', \'(?P<idregione>[^\']+)\', \'(?P<idprovincia>[^\']+)\'\)', home.text):
     campionato = m.group('campionato')
+    #print(m.group('sesso'), m.group('campionato'), m.group('fase'), m.group('codice'), m.group('andata'), m.group('turno'), m.group('idregione'), m.group('idprovincia'), sep='\n')
+    #exit(0)
     if campionato in squadre.keys() and service.existCalendar(squadre[campionato]):
         codice = m.group('codice')
         fase = m.group('fase')
         andata = m.group('andata')
         turno = m.group('turno')
+        sesso = m.group('sesso')
+        idregione = m.group('idregione')
+        idprovincia = m.group('idprovincia')
         print('_' * 30)
         print()
         print(f' {squadre[campionato]} '.center(30, '-'))
         print()
 
         # Scarico la pagina con le date della categoria corrente
-        page = requests.get(f'http://fip.it/AjaxGetDataCampionato.asp?com=RTN&camp={campionato}&fase={fase}&girone={codice}&ar={andata}&turno={turno}')
+        page = requests.post(f'http://fip.it/FipWeb/ajaxRisultatiGetPartite.aspx', data={'com':'RTN', 'sesso': sesso, 'IDRegione':idregione, 'IDProvincia':idprovincia, 'camp':campionato, 'fase':fase, 'girone':codice, 'ar':andata, 'turno':turno})
 
         # Cerco i link di tutte le giornate del campionato
-        for m in re.finditer('getCampionato\(\'RTN\', \'(?P<campionato>.+)\', \'(?P<fase>.+)\', \'(?P<codice>.+)\', (?P<andata>\d), (?P<turno>\d)\)', page.text):
+        for m in re.finditer('getRisultatiPartite\(\'RTN\', \'(?P<sesso>[MF])\', \'(?P<campionato>[^\']+)\', \'(?P<fase>[^\']+)\', \'(?P<codice>[^\']+)\', \'(?P<andata>\d)\', \'(?P<turno>\d)\', \'(?P<idregione>[^\']+)\', \'(?P<idprovincia>[^\']+)\'\)', page.text):
             campionato = m.group('campionato')
             codice = m.group('codice')
             fase = m.group('fase')
             andata = m.group('andata')
             turno = m.group('turno')
-
+            sesso = m.group('sesso')
+            idregione = m.group('idregione')
+            idprovincia = m.group('idprovincia')
             # Scarico la pagina della singola giornata del campionato
-            giornata = requests.get(f'http://fip.it/AjaxGetDataCampionato.asp?com=RTN&camp={campionato}&fase={fase}&girone={codice}&ar={andata}&turno={turno}')
-            tree = html.fromstring(giornata.content)
-
+            giornata = requests.post(f'http://fip.it/FipWeb/ajaxRisultatiGetPartite.aspx', data={'camp':campionato, 'ar':andata, 'com':'RTN', 'fase':fase, 'girone':codice, 'IDProvincia':idprovincia, 'IDRegione':idregione, 'sesso':sesso, 'turno':turno})
+            tree = html.fromstring(f'<div>{giornata.content}</div>')
             # Scorro le partite e individuo squadre, data e luogo
-            games = tree.xpath('div[@class="risTr1"]')
-            places = tree.xpath('div[@class="risTr2"]')
-            for i in range(len(games)):
+            squadre1 = tree.xpath('//td[@class="nome-squadra nome-squadra-1"]')
+            risultati1 = tree.xpath('//td[@class="risultato-squadra risultato-squadra-1"]')
+            risultati2 = tree.xpath('//td[@class="risultato-squadra risultato-squadra-2"]')
+            squadre2 = tree.xpath('//td[@class="nome-squadra nome-squadra-2"]')
+            places = tree.xpath('//td[@class="luogo-arbitri"]')
+            dates = tree.xpath('//td[@class="luogo-arbitri"]/strong/child::text() | //td[@class="luogo-arbitri"]/strong/font/child::text()')
+            for i in range(len(squadre1)):
+                if risultati1[i].text and risultati2[i].text:
+                    result = f'{risultati1[i].text}-{risultati2[i].text}'
+                else:
+                    result = '-'
+                # print(squadre1[i].text, risultati1[i].text, risultati2[i].text, squadre2[i].text, places[i].text, dates[i])
                 # Inizializzo l'oggetto Game
-                game = Game(league=squadre[campionato], gameData=games[i], place=places[i].text.strip())
+                game = Game(
+                    league=squadre[campionato],
+                    teamA=squadre1[i].text,
+                    teamB=squadre2[i].text,
+                    result=result,
+                    place=re.sub(r'[\t\n\r]', '', places[i].text),
+                    dateData=dates[i]
+                )
                 if game.isGardolo or game.isUnder20:
                     if game.futureGame:
                         localEvents.append(game)
                         service.saveGame(game)
+                        print(f'{game.teamA} - {game.teamB}: {game.gameday} {game.time}')
                     else:
                         print(f'{game.teamA} - {game.teamB}: {game.result}')
 
